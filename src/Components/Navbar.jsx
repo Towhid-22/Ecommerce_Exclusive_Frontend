9646028f705f391746db74ab1a71eb0a6def1e3d
Towhidul Islam <towhid212037@gmail.com>
2025-03-17 11:45:16 +0600

    Navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { FiSearch, FiUser } from "react-icons/fi";
+import { GoHeart } from "react-icons/go";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { RiShoppingCart2Line } from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="border-b-[1px] border-black/[30%]">
       <div className="bg-black text-header text-center py-[15px]">
         <div className="container flex items-center justify-end">
           <p>
             Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-            <span className="ml-[8px] font-Poppins font-semibold leading-6">
+            <span className="ml-[8px] font-Poppins font-semibold leading-6 hover:underline">
               ShopNow
             </span>
           </p>
@@ -18,7 +21,44 @@ const Navbar = () => {
           </p>
         </div>
       </div>
-      <div className="container"></div>
+      <div className="container flex items-center justify-between mt-10 mb-4">
+        <img src="Exclusive.png" alt="" />
+        <ul className="flex items-center">
+          <li className="text-base text-black font-Poppins leading-6 mr-12 hover:underline">
+            Home
+          </li>
+          <li className="text-base text-black font-Poppins leading-6 mr-12 hover:underline">
+            Contact
+          </li>
+          <li className="text-base text-black font-Poppins leading-6 mr-12 hover:underline">
+            Contact
+          </li>
+          <li className="text-base text-black font-Poppins leading-6 hover:underline">
+            Sign Up
+          </li>
+        </ul>
+        <div className="flex items-center">
+          <div className="relative mr-6">
+            <input
+              type="text"
+              placeholder="What are you looking for?"
+              className="font-Poppins leading-[18px] pl-5 rounded text-[12px] text-black/[50%] placeholder:text-black/[50%] p-2 bg-searchBg w-[243px] outline-none"
+            />
+            <FiSearch className="absolute top-[6px] right-[12px] w-6 h-6 " />
+          </div>
+          <ul className="flex">
+            <li>
+              <GoHeart className="w-8 h-8 ml-4" />
+            </li>
+            <li>
+              <RiShoppingCart2Line className="w-8 h-8 ml-4" />
+            </li>
+            {/* <li>
+              <FiUser className="w-8 h-8 ml-4" />
+            </li> */}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
